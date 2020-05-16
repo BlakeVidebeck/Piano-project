12e9c57259bde458fa3b972ef32b05dcb52de17a
BlakeVidebeck <blakevidebeck@gmail.com>
2020-05-16 23:28:38 +1000

    made responsive and changed the id for keys

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,6 @@
 // The keys and notes variables store the piano keys
-const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 
-'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 
-'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
+const keys = ['c', 'd', 'e', 'f', 'g', 'a', 'b', 'high-c',
+ 'c-sharp', 'd-sharp', 'f-sharp', 'g-sharp', 'a-sharp'];
 
 const notes = [];
 
@@ -9,16 +8,29 @@ const notes = [];
 keys.forEach(key => notes.push(document.getElementById(key)));
 
 const keyPlay = () => event.target.style.backgroundColor = 'black';
-
 const keyReturn = () => event.target.style.backgroundColor = ''
 
-const play = (note) =>{
+//for desktop
+ const play = (note) =>{
   note.onmousedown = () => {
     keyPlay();
   }
   note.onmouseup = () =>{
     keyReturn();
   }
+} 
+
+//for phone
+const touch = (note) =>{
+  note.ontouchstart = () => {
+    keyPlay();
+  }
+  note.ontouchend = () =>{
+    keyReturn();
+  }
 }
 
+
+
+notes.forEach(touch);
 notes.forEach(play);
